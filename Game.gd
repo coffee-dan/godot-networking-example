f143891ade3c5af6c02ac6cf375c265043a032ed
Daniel <coffee-dan@protonmail.com>
2025-04-12 21:14:25 -0500

    bump from 4.3 to 4.4 and add basic main menu

--- Game.gd
+++ Game.gd
@@ -1,2 +1,13 @@
 extends Node
 
+func _ready():
+	# Create client.
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(IP_ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
+
+	# Create server.
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(4000, 4)
+	multiplayer.multiplayer_peer = peer
+		
